6f4f1e72aa24339a9c83ef37484c65de62184cbd
keitax <keitax913@yahoo.co.jp>
2018-10-06 16:25:51 +0900

    Add a Spring App

---
+++ src/main/java/jp/keitaxdev/basicbatch/BasicBatchApplication.java
@@ -0,0 +1,22 @@
+package jp.keitaxdev.basicbatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBatchApplication implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicBatchApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicBatchApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("hello, basic-batch");
+    }
+}
---
+++ src/test/java/jp/keitaxdev/basicbatch/BasicBatchApplicationTests.java
@@ -0,0 +1,11 @@
+package jp.keitaxdev.basicbatch;
+
+import org.junit.jupiter.api.Test;
+
+class BasicBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
